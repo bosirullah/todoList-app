521161e0f1a2d1f22289b9a5b93725c04e54a0a0
bosirullah <mdbosirullah@gmail.com>
2023-10-02 12:48:04 +0530

    nodemailer bug fixed

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,10 +2,11 @@ const nodemailer = require('nodemailer');
 require("dotenv").config();
 
 const smtpPass = process.env.SMTPPASS;
+const PORT = process.env.port || 25;
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 25,
+    port: PORT,
     auth: {
         user: 'todoListOfficial5657@gmail.com',
         pass: smtpPass
--- models/User.js
+++ models/User.js
@@ -1,12 +1,13 @@
-const mongoose = require("mongoose");
-const passportLocalMongoose = require("passport-local-mongoose");
+const mongoose = require("mongoose"); 
+const passportLocalMongoose = require("passport-local-mongoose"); // Import passport-local-mongoose for simplifying Passport.js integration
 
 const userSchema = new mongoose.Schema({
     username: String,
     password: String, // This will be handled by passport-local-mongoose
+    // Note: The actual password hashing and authentication logic will be handled by passport-local-mongoose
 });
 
-// Use passport-local-mongoose as a plugin to your schema
+// Use passport-local-mongoose as a plugin to your schema to extend the schema with additional functionality
 userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
