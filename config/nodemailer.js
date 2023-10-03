4811a4b6c8cc4750a205dc7f337c5f2bd2c2b0df
bosirullah <mdbosirullah@gmail.com>
2023-10-03 19:21:38 +0530

    nodemailer updated v2

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,12 +2,13 @@ const nodemailer = require('nodemailer');
 require("dotenv").config();
 
 const smtpPass = process.env.SMTPPASS;
-const PORT = process.env.port || 25;
+const PORT = process.env.port || 465;
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     host: 'smtp.gmail.com', 
     port: PORT,
+    secure: true,
     auth: {
         user: 'todoListOfficial5657@gmail.com',
         pass: smtpPass
